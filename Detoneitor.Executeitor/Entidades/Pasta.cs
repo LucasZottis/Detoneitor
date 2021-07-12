17b6bf5b47e94757fa167153fd11ec14ed201587
LucasZottis <zottis.lucas@hotmail.com>
2021-07-11 23:03:26 -0300

    Ajustes.

---
+++ Detoneitor.Executeitor/Entidades/Pasta.cs
@@ -0,0 +1,86 @@
+﻿using BibliotecaPublica.Classes.Gerenciadores;
+using BibliotecaPublica.Classes.Verificadores;
+using System.IO;
+using System.Text;
+
+namespace Detoneitor.Executeitor.Entidades
+{
+    class Pasta
+    {
+        #region Atributos
+
+        private StringBuilder _mensagemValidacao = new StringBuilder();
+        private GerenciadorConfiguracao _gerenciador = new GerenciadorConfiguracao();
+
+        #endregion Atributos
+
+        #region Propriedades
+
+        public int Codigo { get; private set; }
+
+        public string NomePasta { get; private set; }
+
+        public string Caminho { get; private set; }
+
+        #endregion Propriedades
+
+        #region Construtores
+
+        public Pasta(int codigo, string nomePasta, string caminho)
+        {
+            if (codigo < 1)
+            {
+                _mensagemValidacao.AppendLine(" - Código inválido.");
+            }
+
+            if (caminho.TemConteudo() && Directory.Exists(caminho))
+            {
+                _mensagemValidacao.AppendLine(" - Pasta informada não existe.");
+            }
+
+            if (nomePasta.TemConteudo())
+            {
+                _mensagemValidacao.AppendLine(" - Não foi informado o nome da pasta.");
+            }
+
+            if (!_mensagemValidacao.TemConteudo())
+            {
+                Codigo = codigo;
+                NomePasta = nomePasta;
+                Caminho = caminho;
+            }
+        }
+
+        #endregion Construtores
+
+        #region Métodos Públicos
+
+        public string Validar()
+        {
+            return _mensagemValidacao.ToString();
+        }
+
+        #endregion Métodos Públicos
+
+        #region Métodos Sobrescritos
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            if (!(obj is Pasta))
+            {
+                return false;
+            }
+
+            Pasta pastaInformada = obj as Pasta;
+
+            return this.Codigo.Igual(pastaInformada.Codigo);
+        }
+
+        #endregion Métodos Sobrescritos
+    }
+}
---
+++ Detoneitor.Executeitor/Repositorio/RepositorioPastas.cs
@@ -0,0 +1,70 @@
+﻿using Detoneitor.Executeitor.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Detoneitor.Executeitor.Repositorio
+{
+    class RepositorioPastas
+    {
+        #region Atributos
+
+        private List<Pasta> _listaInterna = null;
+
+        #endregion Atributos
+
+        #region Propriedades
+
+        public IList<Pasta> Pastas
+        {
+            get
+            {
+                if (_listaInterna == null)
+                {
+                    _listaInterna = new List<Pasta>();
+                }
+
+                return _listaInterna;
+            }
+
+            private set { }
+        }
+
+        public int Contagem
+        {
+            get
+            {
+                if (_listaInterna == null)
+                {
+                    _listaInterna = new List<Pasta>();
+                }
+
+                return _listaInterna.Count;
+            }
+        }
+
+        #endregion Propriedades
+
+        #region Construtores
+
+
+
+        #endregion Construtores
+
+        #region Métodos Públicos
+
+        public void AdicionarPasta(Pasta pasta)
+        {
+            _listaInterna.Add(pasta);
+        }
+
+        public void RemoverPasta(Pasta pasta)
+        {
+            _listaInterna.Remove(pasta);
+        }
+
+        #endregion Métodos Públicos
+    }
+}
